ba0332dd45cf61cc050215ce8c089c3eb020beb8
BT_1N3_17 <itsbenwu>
2019-04-05 08:17:06 -0400

    Helpp

--- src/com/company/Digits.java
+++ src/com/company/Digits.java
@@ -6,12 +6,11 @@ public class Digits {
 public Digits(int number){
     this.number=number;
 }
-    public ArrayList<Integer> arr(int number){
+    public ArrayList<Integer> arr(){
         ArrayList<Integer> phart=new ArrayList<>();
-        for(int mod=10; number!=0; mod=mod*10 ){
+        for(int mod=10; number!=0; number=number/10 ){
             int remainder=number%mod;
             phart.add(0,remainder);
-            number=-remainder;
         }
         return phart;
     }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         Digits l = new Digits(2434);
-System.out.println(l.arr(10));
+System.out.println(l.arr());
     }
 }
