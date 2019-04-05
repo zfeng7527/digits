4f8e0347aac3a9a7a7a344964d6e443bfa2b2b85
BT_1N3_17 <itsbenwu>
2019-04-05 08:20:41 -0400

    Helpp

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         Digits l = new Digits(2434);
-System.out.println(l.arr());
+    System.out.println(l.arr());
+        Digits m = new Digits(599343405);
+        System.out.println(m.arr());
     }
 }
